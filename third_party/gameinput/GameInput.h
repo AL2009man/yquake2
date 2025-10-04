d5fc7d872535de3785b57e2500b97c6ca8bb0109
AL2009man <67606569+AL2009man@users.noreply.github.com>
2025-10-03 22:18:08 -0400

    remove most of the junk

--- third_party/gameinput/GameInput.h
+++ third_party/gameinput/GameInput.h
@@ -1,11 +1,5 @@
 /*
- * GameInput C Compatibility Wrapper
- * 
- * This header tricks the original GameInput.h into thinking we're compiling C++
- * so SDL3 can detect GameInput support, while keeping our project as C.
- * 
- * We temporarily define __cplusplus to bypass the C++ requirement check,
- * then include minimal definitions that SDL3 needs for detection.
+ * GameInput C Wrapper - enables SDL3 GameInput detection in C projects
  */
 
 #pragma once
@@ -13,18 +7,14 @@
 #ifndef GAMEINPUT_C_WRAPPER_H
 #define GAMEINPUT_C_WRAPPER_H
 
-// Save the current state of __cplusplus
 #ifdef __cplusplus
 #define GAMEINPUT_WAS_CPP_DEFINED
 #else
-// Temporarily define __cplusplus to bypass the GameInput.h C++ check
 #define __cplusplus 1
 #endif
 
-// Include the real GameInput.h now that __cplusplus is defined
 #include "include/GameInput.h"
 
-// Restore the original __cplusplus state
 #ifndef GAMEINPUT_WAS_CPP_DEFINED
 #undef __cplusplus
 #endif
